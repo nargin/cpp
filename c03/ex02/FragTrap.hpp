42b8ea664537791cd2abd7938aae12eb0bd6759d
nrgn <mejumine@gmail.com>
2023-06-14 15:51:13 +0200

    heritage hihi facile

--- c03/ex02/FragTrap.hpp
+++ c03/ex02/FragTrap.hpp
@@ -1,44 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: romaurel <romaurel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/14 01:47:59 by robin             #+#    #+#             */
-/*   Updated: 2023/06/14 15:41:48 by romaurel         ###   ########.fr       */
+/*   Created: 2023/06/14 15:47:07 by romaurel          #+#    #+#             */
+/*   Updated: 2023/06/14 15:50:19 by romaurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SCAVTRAP_HPP
-# define SCAVTRAP_HPP
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
 
 #include <iostream>
 #include "ClapTrap.hpp"
 
-class	ScavTrap : public ClapTrap {
+class	FragTrap : public ClapTrap {
 	public :
-		ScavTrap(std::string name) : ClapTrap(name) {
-			std::cout << "ScavTrap " << name << " created" << std::endl;
+		FragTrap(std::string name) : ClapTrap(name) {
+			std::cout << "FragTrap " << name << " created" << std::endl;
 			this->_hitPoints = 100;
-			this->_energyPoints = 50;
-			this->_attackDamage = 20;
+			this->_energyPoints = 100;
+			this->_attackDamage = 30;
 		};
-		ScavTrap(ScavTrap const& src) : ClapTrap(src) {
-			std::cout << "ScavTrap " << src._name << " created by copy" << std::endl;
+		FragTrap(FragTrap const& src) : ClapTrap(src) {
+			std::cout << "FragTrap " << src._name << " created by copy" << std::endl;
 			*this = src;
 			this->_name += "_copy";
 		};
-		~ScavTrap(void) {
-			std::cout << "ScavTrap " << this->_name << " destroyed" << std::endl;
+		~FragTrap(void) {
+			std::cout << "FragTrap " << this->_name << " destroyed" << std::endl;
 		};
 
-		ScavTrap&	operator=(ScavTrap const& src);
+		FragTrap&	operator=(FragTrap const& src);
 
 		void		attack(std::string const& target);
 		void		takeDamage(unsigned int amount);
 		void		beRepaired(unsigned int amount);
-		void		guardGate(void);
+		void		highFivesGuys(void);
 	private :
 		std::string	_name;
 		int			_hitPoints;
--- c03/ex02/main.cpp
+++ c03/ex02/main.cpp
@@ -6,17 +6,19 @@
 /*   By: romaurel <romaurel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 01:21:58 by robin             #+#    #+#             */
-/*   Updated: 2023/06/14 15:41:21 by romaurel         ###   ########.fr       */
+/*   Updated: 2023/06/14 15:49:28 by romaurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
 int main(void)
 {
     ClapTrap clapTrap("Clappy");
     ScavTrap scavTrap("Cafard");
+    FragTrap fragTrap("Fraggy");
 
     clapTrap.attack("China");
     clapTrap.takeDamage(10);
@@ -26,5 +28,10 @@ int main(void)
     scavTrap.takeDamage(10);
     scavTrap.beRepaired(10);
     scavTrap.guardGate();
+
+    fragTrap.attack("Mexico");
+    fragTrap.takeDamage(10);
+    fragTrap.beRepaired(10);
+    fragTrap.highFivesGuys();
     return (0);
 }
