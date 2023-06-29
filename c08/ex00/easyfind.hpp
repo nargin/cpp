e2d28fbb2be0e141041985b0290f1fa7d60c71d0
nrgn <mejumine@gmail.com>
2023-06-29 15:19:45 +0200

    need more test

--- c08/ex00/easyfind.hpp
+++ c08/ex00/easyfind.hpp
@@ -3,12 +3,19 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <exception>
+
+class NoFind : public std::exception {
+	public :
+		virtual const char *what(void) const throw() { return "No finding..."; };
+};
 
 template <typename T>
 typename T::iterator easyfind(T &container, int value)
 {
 	typename T::iterator it = std::find(container.begin(), container.end(), value);
-	if (it == container.end())
-		throw std::exception();
+	if (it == container.end()) {
+		throw NoFind();
+	}
 	return it;
 }
--- c08/ex00/main.cpp
+++ c08/ex00/main.cpp
@@ -4,8 +4,16 @@ int main()
 {
 	std::vector<int> v;
 	for (int i = 0; i < 10; i++)
-		v.push_back(i);
-	std::vector<int>::iterator it = easyfind(v, 5);
-	std::cout << *it << std::endl;
+		v.push_back(i + 2);
+	std::cout << "vector: ";
+	for (std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	try {
+		std::vector<int>::iterator it = easyfind(v, 3);
+		std::cout << *it << std::endl;
+	} catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
 	return 0;
 }
