82b64298a1160c6a0749efe96411b68a1c18633f
nrgn <robin@DESKTOP-G9TJA47>
2023-07-02 17:32:27 +0200

    las pokemones

---
+++ c08/ex02/MutantStack.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stack>
+#include <iostream>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack() : std::stack<T>() {}
+		MutantStack(const MutantStack<T> &copy) : std::stack<T>(copy) {}
+		MutantStack<T> &operator=(const MutantStack<T> &copy) {
+			if (this == &copy)
+				return (*this);
+			std::stack<T>::operator=(copy);
+			return (*this);
+		}
+		virtual ~MutantStack() {}
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		iterator begin() { return (std::stack<T>::c.begin()); }
+		iterator end() { return (std::stack<T>::c.end()); }
+};
