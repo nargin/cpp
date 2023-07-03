c37f68fa67fa547de8a432c3b6ebf77d0ee342d3
nrgn <none>
2023-07-03 22:42:55 +0200

    nueve peligro

---
+++ c09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+
+class BitcoinExchange {
+	private :
+		std::map<std::string, double> _data;
+}
---
+++ c09/ex00/main.cpp
@@ -0,0 +1,8 @@
+#include "BitcoinExchange.hpp"
+
+int	main(int ac, char *av[]){
+	if (ac != 2){
+		std::cout << "Usage: ./bitcoin [file]" << std::endl;
+		return (1);
+	}
+}
