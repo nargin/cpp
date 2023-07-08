01a71b263595c8d8d8e0b33d6cc089280981e1d5
nrgn <none>
2023-07-08 20:21:12 +0200

    no work done

--- c09/ex00/BitcoinExchange.hpp
+++ c09/ex00/BitcoinExchange.hpp
@@ -11,6 +11,31 @@
 #include <cmath>
 
 class BitcoinExchange {
-	private :
+	public :
 		std::map<std::string, double> _data;
-}
+		BitcoinExchange(std::string filename) {
+			std::ifstream file(filename.c_str());
+			if (!file.is_open()){
+				std::cout << "Error: could not open file" << std::endl;
+				exit(1);
+			}
+			std::string line;
+			while (std::getline(file, line)){
+				std::cout << line << std::endl;
+				std::stringstream ss(line);
+				std::cout << ss.str() << std::endl;
+				std::string date;
+				double price;
+				ss >> date >> price;
+				_data[date] = price;
+			}
+			file.close();
+		};
+		~BitcoinExchange(){};
+		void print(){
+			std::cout << "Date\t\tPrice" << std::endl;
+			for (std::map<std::string, double>::iterator it = _data.begin(); it != _data.end(); it++){
+				std::cout << it->first << "; " << it->second << std::endl;
+			}
+		};
+};
--- c09/ex00/main.cpp
+++ c09/ex00/main.cpp
@@ -2,7 +2,11 @@
 
 int	main(int ac, char *av[]){
 	if (ac != 2){
-		std::cout << "Usage: ./bitcoin [file]" << std::endl;
+		std::cout << "Error: Could not open file" << std::endl << "Usage: ./btc [file]" << std::endl;
 		return (1);
 	}
+	BitcoinExchange exchange(av[1]);
+	std::cout << "exchange print" << std::endl;	
+	exchange.print();
+	return (0);
 }
